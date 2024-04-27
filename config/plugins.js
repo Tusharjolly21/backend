f5baab1812a935b3fb4b4b999d2e9e2d8ed57830
Tushar Jolly <tushar.gts7650@gmail.com>
2024-04-27 09:42:18 +0530

    first commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,1 @@
-const crypto = require("crypto");
-
-module.exports = ({ env }) => ({
-  "users-permissions": {
-    config: {
-      jwtSecret: env("JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
-    },
-  },
-});
+module.exports = () => ({});
